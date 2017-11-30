c66817affb493fe488ba794062c6062f91ba18d4
Matt Hardman <matthardman@matts-mbp.home>
2017-11-30 20:17:43 +0000

    socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const http = require('http');
+const socket = require('socket.io')
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
 
-let app = express();
 const port = process.env.PORT || 3000
+let app = express();
+let server = http.createServer(app);
+let io = socket(server);
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('new user');
+
+	socket.on('disconnect', () => {
+		console.log('bye')
+	});
+});
+
+
+
+server.listen(port, () => {
 	console.log(`listening on ${port}`)
-})
+});
